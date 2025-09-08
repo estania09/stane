fb74091f490120608c8104be0e1e6969ae892d0b
estania09 <staniacornet9@gmail.com>
2025-09-08 00:01:27 -0400

    commit

--- Cornet_project/app/src/main/java/com/example/stane/MainActivity.kt
+++ Cornet_project/app/src/main/java/com/example/stane/MainActivity.kt
@@ -9,9 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
         flashcardQuestion.setOnClickListener {
