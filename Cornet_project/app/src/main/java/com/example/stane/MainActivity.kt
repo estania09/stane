bdef2308ef9101bb320c428b1e865debf39b9f1e
estania09 <staniacornet9@gmail.com>
2025-09-08 00:03:44 -0400

    commit

--- Cornet_project/app/src/main/java/com/example/stane/MainActivity.kt
+++ Cornet_project/app/src/main/java/com/example/stane/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.stane
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
